81f346e3b01de9db54386ed5c7ce9afe6d2299e7
ogogost <galsan7@gmail.com>
2019-12-24 15:00:29 +0300

    17-39

--- 4.py
+++ 4.py
@@ -53,3 +53,17 @@ for i in range(len(value)):
         count = count + 1
         print(value[i], count)
 
+# сжать строку по правилу, все последовательности повторяющихся символов длины больше 1 заменить на символ + кол-во повторений
+
+value = 'aaaaccccccbbbbbbb' + '@'
+
+count = 0
+
+result = ''
+
+for i in range(len(value) - 1):
+    if value[i] != value[i + 1]:
+        current_value = (value[count: i + 1])
+        result += value[i] + str(len(current_value))
+        count = i + 1
+print(result)
