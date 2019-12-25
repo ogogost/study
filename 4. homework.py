980dc44586588a8e66cfb86ad1a84123a7a57ce8
ogogost <galsan7@gmail.com>
2019-12-25 17:25:11 +0300

    17-25

---
+++ 4. homework.py
@@ -0,0 +1,36 @@
+N = 10
+data = [0 for i in range(N)]
+count_zero = 0
+
+def park(n):
+    for i in range(N):
+        if data.count(0) == 0:
+            print('No free')
+            break
+
+        if data[i] != 0:
+            data[i + 1] = n
+
+    print(data)
+
+def leave(n):
+    data.remove(n)
+    print(data)
+
+def free():
+   print(data.count(0))
+
+def show():
+    print(data)
+
+while True:
+    com = ''
+    com, n = input('Input command:').split()
+    if com == 'park':
+        park(n)
+    elif com == 'leave':
+        leave(n)
+    elif com == 'free':
+        free()
+    elif com == 'show':
+        show()
