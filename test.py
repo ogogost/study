1af576790254bacafcdc9f2cd5478e3c49c29efd
Alex <austina1992@gmail.com>
2020-01-22 21:34:59 +0300

    22.01.2020 21-34

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-for i in range(3):
-    print(i)
+# data = [1, 2, 3]
+# data.remove(3)
+# for i in data:
+#     if data[i] == 2:
+#         data.remove(i)
+# print(data)
 
+class Test:
+
+    def __init__(self, id):
+        self.id = id
+
+x = Test()
+print(x)
+
+data = []
+for i in range(3):
+    data.append(Test(i))
+
+print(data)
 
